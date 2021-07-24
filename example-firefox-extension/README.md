0c45d5adc60e5436301203be45d0e8c6e1a894c2
Eirik Brandtzæg <eirikb@eirikb.no>
2021-07-24 23:53:07 +0200

    Firefox extension demo

---
+++ example-firefox-extension/README.md
@@ -0,0 +1,11 @@
+Example client
+
+This is also the client hosted at https://eirikb.github.io/callsign .  
+Using Firebase, but could use anything.
+
+Run locally (for dev):
+
+```bash
+npm i
+npm start
+```
---
+++ example-firefox-extension/src/app.tsx
@@ -0,0 +1,71 @@
+import { data, don, init, pathOf, React } from "../../example-client/src/dd";
+import { Chat, Init } from "../../example-client/src/views";
+import { createLogger, toColor } from "../../example-client/src/log";
+
+init(
+  document.body,
+  <div>
+    {don(pathOf().panel).map<Panel>((panel) => {
+      switch (panel) {
+        case "chat":
+          return <Chat />;
+        default:
+          return <Init />;
+      }
+    })}
+    <hr />
+
+    <b>Incoming:</b>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const incoming = e.target.incoming.value;
+          try {
+            data.incoming = JSON.parse(incoming);
+            data.incoming = undefined;
+            e.target.incoming.value = "";
+          } catch (e) {
+            createLogger("").error(e);
+          }
+        }}
+      >
+        <input name="incoming" type="text" />
+        <button>Process</button>
+      </form>
+    </div>
+    <b>Outgoing:</b>
+    <div>
+      {don(pathOf().outgoing!).map((outgoing) => {
+        const input = <input type="text" value={JSON.stringify(outgoing)} />;
+        return (
+          <div>
+            {input}
+            <button
+              type="button"
+              onClick={() => {
+                input.select();
+                document.execCommand("copy");
+              }}
+            >
+              Copy
+            </button>
+          </div>
+        );
+      })}
+    </div>
+
+    <hr />
+    <div>
+      {don(pathOf().log.$).map<Log>((log) => (
+        <div style={{ color: toColor(log) }}>
+          [{log.stamp.toISOString()}]{" "}
+          {log.callsign ? (
+            <span style="color: blue">{log.callsign}</span>
+          ) : null}{" "}
+          {log.text}
+        </div>
+      ))}
+    </div>
+  </div>
+);
